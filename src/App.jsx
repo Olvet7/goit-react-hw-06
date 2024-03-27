966a3390a55dc06ba8b5789adff497c1d5d4f13c
Olvet7 <volya.maslova@gmail.com>
2024-03-28 00:12:01 +0200

    1

--- src/App.jsx
+++ src/App.jsx
@@ -8,84 +8,9 @@ export default function App() {
   return (
     <div>
       <h2>Phonebook</h2>
-      {/* <ContactForm/>
-      <ContactList/>
-      <SearchBox/> */}
       <ContactForm />
       <ContactList />
       <SearchBox />
     </div>
   )
 }
-
-
-// export default function App() {
-//   const [search, setSearch] = useState("");
-//   const [contacts, setContacts] = useState(() => {
-//     const savedContacts = JSON.parse(window.localStorage.getItem("contacts"));
-//     return (
-//       savedContacts || [
-//         { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-//       ]
-//     );
-//   });
-
-//   useEffect(() => {
-//     const savedContacts = JSON.parse(window.localStorage.getItem("contacts"));
-//     if(savedContacts) {
-//       setContacts(savedContacts)
-//     } 
-//   }, []);
-
-//   useEffect(() => {
-//     window.localStorage.setItem("contacts", JSON.stringify(contacts))
-//   }, [contacts])
-
-  
-
-//   const handleSearchChange = (value) => {
-//     setSearch(value);
-//   };
-
-//   const handleAddContact = (newContact) => {
-//     const isDuplicateName = contacts.some(
-//       (contact) => contact.name === newContact.name
-//     );
-//     if (isDuplicateName) {
-//       Notify.warning("Already in list", {
-//         width: "200px",
-//         fontSize: "20px",
-//         position: "right-top",
-//         distance: "50px",
-//       });
-//       return;
-//     }
-
-//     const newContactWithId = { id: nanoid(), ...newContact };
-//     const updatedContacts = [newContactWithId, ...contacts];
-//     setContacts(updatedContacts);
-//     window.localStorage.setItem("contacts", JSON.stringify(updatedContacts));
-//   };
-
-//   const deleteContact = (id) => {
-//     const updatedContacts = contacts.filter((contact) => contact.id !== id);
-//     setContacts(updatedContacts);
-//     window.localStorage.setItem("contacts", JSON.stringify(updatedContacts));
-//   };
-
-//   const filteredContacts = contacts.filter((contact) =>
-//     contact.name.toLocaleLowerCase().includes(search.toLocaleLowerCase())
-//   );
-
-//   return (
-//     <>
-//       <h3>Phonebook</h3>
-//       <SearchBox value={search} onChange={handleSearchChange} />
-//       <ContactForm onAddContact={handleAddContact} />
-//       <ContactList
-//         contacts={filteredContacts}
-//         onDeleteContact={deleteContact}
-//       />
-//     </>
-//   );
-// }
